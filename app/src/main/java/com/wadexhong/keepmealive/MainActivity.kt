e7d39ea19b0d69aea3779e141b6a1929ad57fa25
wadehong <wade.hong@hancock.pro>
2021-04-12 13:46:32 +0800

    feat: Add sample app

---
+++ app/src/main/java/com/wadexhong/keepmealive/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.wadexhong.keepmealive
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (Helper.isCustomPermissionSettingSupport(this)) {
+            if (Helper.launchPermissionSettingPage(this)) {
+                Toast.makeText(
+                    this,
+                    "Success",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                Toast.makeText(
+                    this,
+                    "Failed",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        } else {
+            Toast.makeText(
+                this,
+                "This devices is not support by this library",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+}
